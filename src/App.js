c40b790b64985ca9307493ac8c452b9c7e3ec7f7
john-franti <john.franti@flatironschool.com>
2020-02-20 15:38:04 -0800

    async redux set up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
 import logo from "./donut.png";
 import "./App.css";
+import DonutContainer from "./components/DonutContainer";
+import DonutBrowser from "./components/DonutBrowser";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 class App extends Component {
-
-  state = {
-    loading: false,
-    donuts: [],
-    error: ''
-  }
-
-  // FETCH_DONUTS_REQUEST loading:true
-  // FETCH_DONUTS_SUCCESS loading:false, donuts to data
-  // FETCH_DONUTS_ERROR loading:false, error to message
+  state = {};
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Welcome to the Donut Shop</h1>
-          <img className="App-logo" src={logo} alt="logo"/>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <h1>Welcome to the Donut Shop</h1>
+            <img className="App-logo" src={logo} alt="logo" />
+            <DonutContainer />
+            <DonutBrowser />
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
