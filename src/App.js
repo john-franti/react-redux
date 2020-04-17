289a97aaf629f1c2d2444584492f01945e31b39c
John <Franti>
2020-04-17 11:33:18 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 class App extends Component {
-  state = {};
-
   render() {
     return (
       <Provider store={store}>
         <div className="App">
-          <header className="App-header">
-            <h1>Welcome to the Donut Shop</h1>
-            <img className="App-logo" src={logo} alt="logo" />
-            <DonutContainer />
-            <DonutBrowser />
+          <header className="App-header parent">
+            <div className="div1">
+              <h1>Welcome to the Donut Shop</h1>
+              <img className="App-logo" src={logo} alt="logo" />
+              <DonutContainer />
+            </div>
+            <div className="div2">
+              <DonutBrowser />
+            </div>
           </header>
         </div>
       </Provider>
